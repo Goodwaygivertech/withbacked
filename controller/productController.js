d919630f352a5494040aea779b3e6b395a7c11ab
GoodWayGiverTech <goodwaygiver143@gmail.com>
2023-06-03 11:47:31 +0530

    i created a many function for crud opration

--- controller/productController.js
+++ controller/productController.js
@@ -1,42 +1,80 @@
-const fs = require("fs");
-// const index = fs.readFileSync('index.html', 'utf-8');
-// const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-// const products = data.products;
 const Product = require("../model/productSchema");
-console.log(Product)
+// console.log(Product);
 
-exports.createProduct = (req, res) => {
-  // console.log(req.body);
-  products.push({ title: req.body });
-  res.status(201).json({success:"ho gya success"},req.body);
+//here i am creating the product and save into the database
+exports.createProduct = async (req, res) => {
+  try {
+    let product = new Product(req.body);
+    const savedProduct = await product.save();
+    res.status(201).json({ success: "ho gya success" }, savedProduct);
+  } catch (error) {
+    res.status(500).send("some internal error occured");
+  }
 };
 
-exports.getAllProducts = (req, res) => {
-  res.json(products);
+// here getting all products from database.
+exports.getAllProducts = async (req, res) => {
+  try {
+    res.json(await Product.find());
+  } catch (error) {
+     res.status(500).send("some internal error occured");
+
+  }
 };
 
-exports.getProduct = (req, res) => {
-  const id = +req.params.id;
-  const product = products.find((p) => p.id === id);
-  res.json(product);
+//here getting the single product using id.
+exports.getProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const product = await Product.findById({ _id: id });
+    res.json(product);
+  } catch (error) {
+     res.status(500).send("some internal error occured");
+
+  }
 };
-exports.replaceProduct = (req, res) => {
-  const id = +req.params.id;
-  const productIndex = products.findIndex((p) => p.id === id);
-  products.splice(productIndex, 1, { ...req.body, id: id });
-  res.status(201).json({success:"ho gya"});
+
+//here we are replacing the database product,by this all data of database will remove and req.body data will arive there.
+exports.replaceProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const replacedProduct = await Product.findOneAndReplace(
+      { _id: id },
+      req.body,
+      { new: true }
+    );
+    res.status(201).json({ success: "ho gya", replacedProduct });
+  } catch (error) {
+     res.status(500).send("some internal error occured");
+
+  }
 };
-exports.updateProduct = (req, res) => {
-  const id = +req.params.id;
-  const productIndex = products.findIndex((p) => p.id === id);
-  const product = products[productIndex];
-  products.splice(productIndex, 1, { ...product, ...req.body });
-  res.status(201).json({success:"ho gya"});
+
+//here we only updatting that thing witch is in req.body
+exports.updateProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedProduct = await Product.findByIdAndUpdate(
+      { _id: id },
+      req.body,
+      { new: true }
+    );
+
+    res.status(201).json({ success: "ho gya", updatedProduct });
+  } catch (error) {
+     res.status(500).send("some internal error occured");
+
+  }
 };
-exports.deleteProduct = (req, res) => {
-  const id = +req.params.id;
-  const productIndex = products.findIndex((p) => p.id === id);
-  const product = products[productIndex];
-  products.splice(productIndex, 1);
-  res.status(201).json(product);
+
+// deleting the daa here using id
+exports.deleteProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const product = await Product.findByIdAndDelete({ _id: id });
+    res.status(201).json({ delete: "productDeleted", product });
+  } catch (error) {
+     res.status(500).send("some internal error occured");
+
+  }
 };
