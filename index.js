af170cd196451390c9195b2dda76109746bf832b
Good Way Giver Tech <116709996+Goodwaygivertech@users.noreply.github.com>
2023-06-05 12:45:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(cors());
 
 const productRouter = require("./routes/product");
-const userRouter = require("./routes/user");
 // console.log("env", process.env.DB_PASSWORD);
 
 // connectToMongo
@@ -20,7 +19,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 app.use("/products", productRouter.router);
-app.use("/users", userRouter.router);
+
 
 app.listen(Port, () => {
   console.log("app started on ", Port);
